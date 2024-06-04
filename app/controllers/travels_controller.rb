4eca036bb0fb029e6292548a1ddca7fba1240dcd
Benoit LEDEUIL <benoitledeuil@gmail.com>
2024-06-04 17:06:48 +0200

    logique de filtre sur l'index des travels

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,8 +1,14 @@
 class TravelsController < ApplicationController
   def index
-    raise
-    
     @travels = Travel.all
+
+    @travels = @travels.where(airport_start: params[:search][:airport_start]) if params[:search][:airport_start].present?
+    @travels = @travels.where(start_date: params[:search][:start_date]) if params[:search][:start_date].present?
+    @travels = @travels.where(budget_max: params[:search][:budget_max]) if params[:search][:budget_max].present?
+
+    # search = Search.new(search_params)
+    # search.user = current_user
+    # search.save
   end
 
   def new
@@ -10,13 +16,13 @@ class TravelsController < ApplicationController
   end
 
   def create
-    @travel = Travel.new(travels_params)
+    @travel = Travel.new(search_params)
     @travel.save
   end
 
   private
 
-  def travels_params
-    params.require(:travel).permit(:airport_start, :start_date, :budget_max, :lasting, :destination, :mood, :age, :status)
+  def search_params
+    params.require(:search).permit(:airport_start, :start_date, :budget_max, :lasting, :destination, :mood, :age, :status)
   end
 end
