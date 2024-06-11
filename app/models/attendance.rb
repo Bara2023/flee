07adf2bc1c986cfbdcc61712c2e701880200726c
Benoit LEDEUIL <benoitledeuil@gmail.com>
2024-06-11 17:17:29 +0200

    page mes candidatures

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,4 +3,13 @@ class Attendance < ApplicationRecord
   belongs_to :travel
 
   enum status: %i[pending confirmed rejected]
+
+  def translated_status
+    case status.to_s
+    when 'pending' then 'En attente'
+    when 'confirmed' then 'Confirmé'
+    when 'rejected' then 'Refusé'
+    end
+  end
+
 end
